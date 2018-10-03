2af2446dd2fa1eba29f1bbd3357106ed399bf2dd
hazet42 <herzinger@posteo.de>
2018-10-03 21:40:58 +0200

    Control.py for web and hardware switch input

---
+++ control.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+#
+# Read hardware input / web input
+
+# Import Systemwide libraries:
+
+# Import application modules:
+
+
+# for now (since hardware switches and web switch are not existend):
+
+def onoff_switch():
+    return True
