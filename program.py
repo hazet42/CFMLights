9272ab309653035696dbbbe40400300097670ea8
hazet42 <herzinger@posteo.de>
2018-10-07 20:41:08 +0200

    moved programs to program.py module

---
+++ program.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+# Program Module. Creates color programs
+#
+
+def prog_0():
+# Fixed Color Values
+    wcrgb = [10,0,100,0,0]
+    return (wcrgb)
+
+
+def prog_1():
+    return (10,0,100,0,0)
+
+
+def prog_2():
+    return (10,0,100,0,0)
+
+
+def colors(prog_num):
+# Select the program for the WW/CW RGB Values. This is the switcher, the programs are defines above
+    if prog_num == 0:    
+        wcrgb = [10,0,100,0,0]
+
+    elif prog_num == 1:
+        wcrgb = [10,0,0,0,100]
+
+
+
+
+    return (wcrgb)
+
+
