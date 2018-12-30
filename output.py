66afb296845bf2db7272c8b345b0df23d1cd80a7
hazet42 <herzinger@posteo.de>
2018-12-30 13:37:51 +0100

    Added support for HW PWM via the adafruit_pca9685

--- output.py
+++ output.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
-# Output Module. Set GPIO outputs of Raspberry Pi to display 5 colors
+# Output Module. Set pulse width modulation (PWM) outputs to dim LED lights:
+# Two options, as defined in the config file in pwm_mode:
+#   HW ... use Adafruit Pca9685 - Bord for pulse witdh modulation
+#   SW ... use Raspberry Pi's software PWM
 #
 # Input is set in percentages (0-100) for warmwhite,coldwhite,red,green,blue
 
@@ -8,6 +11,9 @@
 import RPi.GPIO as GPIO
 import time
 import control
+import board
+import busio
+import adafruit_pca9685
 
 
 
@@ -19,8 +25,7 @@ def init_gpio(pwm_freq):
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
 
-# Define GPIO Channels (mainly use the ones that do not have additional functionality):
-# Inputs are pulled DOWN, so they expect 3.3V to be HIGH
+# Define GPIO output Channels if SW PWM is selected: 
 #
 #    5... warmwhite
 #    6... coldwhite
@@ -28,28 +33,45 @@ def init_gpio(pwm_freq):
 #   23... blue
 #   24... red
 #   25... green
-#
+
+    if pwm_mode == "SW":
+        GPIO.setup(5, GPIO.OUT)
+        GPIO.setup(6, GPIO.OUT)
+
+        GPIO.setup(23, GPIO.OUT)
+        GPIO.setup(24, GPIO.OUT)
+        GPIO.setup(25, GPIO.OUT)
+
+        ww = GPIO.PWM(5, pwm_freq)
+        cw = GPIO.PWM(6, pwm_freq)
+        red = GPIO.PWM(24, pwm_freq)
+        green = GPIO.PWM(25, pwm_freq)
+        blue = GPIO.PWM(23, pwm_freq)
+
+
+
+    elif pwm_mode == "HW":
+        i2c = busio.I2C(board.SCL, board.SDA)
+        hat = adafruit_pca9685.PCA9685(i2c)
+        hat.frequency = pwm_freq
+
+        red = hat.channels[6]
+        blue = hat.channels[5]
+        green = hat.channels[4]
+        cw = hat.channels[0]
+        ww = hat.channels[1]
+
+
+
+    gpio=(ww,cw,red,green,blue)
+
+# Set Input GPIO channels for switching
+# Inputs are pulled DOWN, so they expect 3.3V to be HIGH
 #   12... switch on-off
 #   13... switch sunlight-extralight
 
-    GPIO.setup(5, GPIO.OUT)
-    GPIO.setup(6, GPIO.OUT)
-
-    GPIO.setup(23, GPIO.OUT)
-    GPIO.setup(24, GPIO.OUT)
-    GPIO.setup(25, GPIO.OUT)
-
     GPIO.setup(12, GPIO.IN,pull_up_down = GPIO.PUD_DOWN)
     GPIO.setup(13, GPIO.IN,pull_up_down = GPIO.PUD_DOWN)
-
-
-    ww = GPIO.PWM(5, pwm_freq)
-    cw = GPIO.PWM(6, pwm_freq)
-    red = GPIO.PWM(24, pwm_freq)
-    green = GPIO.PWM(25, pwm_freq)
-    blue = GPIO.PWM(23, pwm_freq)
-
-    gpio=(ww,cw,red,green,blue)
 
     return (gpio)
 
@@ -59,11 +81,12 @@ def start_gpio_pwm(gpio):
 
     (ww,cw,red,green,blue)=gpio
 
-    ww.start(0) 
-    cw.start(0) 
-    red.start(0) 
-    green.start(0) 
-    blue.start(0) 
+    if pwm_mode == "SW":
+        ww.start(0) 
+        cw.start(0) 
+        red.start(0) 
+        green.start(0) 
+        blue.start(0) 
 
 
 
@@ -72,11 +95,12 @@ def stop_gpio_pwm(gpio):
 
     (ww,cw,red,green,blue)=gpio
 
-    ww.stop()
-    cw.stop()
-    red.stop()
-    green.stop()
-    blue.stop()
+    if pwm_mode == "SW":
+        ww.stop()
+        cw.stop()
+        red.stop()
+        green.stop()
+        blue.stop()
 
 
 def fade_color(gpio,wcrgb_old,wcrgb):
@@ -98,7 +122,7 @@ def fade_color(gpio,wcrgb_old,wcrgb):
     for i in range (0,100):
         wcrgb_i = [ww_o + i*ww_d, cw_o + i*cw_d, red_o + i*red_d, green_o + i*green_d, blue_o + i*blue_d]
         colors(gpio,wcrgb_i)
-        time.sleep(0.04)
+        time.sleep(0.01)
 
     colors(gpio,wcrgb)
 
@@ -106,9 +130,14 @@ def fade_color(gpio,wcrgb_old,wcrgb):
 
 
 
-def antialias(color):
-    color_r = round(color,1)
-    return (color_r)
+def hexscale(color):
+
+    # Software-Module requires duty cycles between 0 ... 0xfff
+    # this function scales the percentage value (0 ... 100%) to this range
+
+    colorhex = color/100*0xffff
+    return (colorhex)
+
 
 
 def colors(gpio,wcrgb):
@@ -117,8 +146,19 @@ def colors(gpio,wcrgb):
     (ww,cw,red,green,blue)=gpio
     (ww_c,cw_c,red_c,green_c,blue_c) = wcrgb
 
-    ww.ChangeDutyCycle(antialias(ww_c))
-    cw.ChangeDutyCycle(antialias(cw_c))
-    red.ChangeDutyCycle(antialias(red_c))
-    green.ChangeDutyCycle(antialias(green_c))
-    blue.ChangeDutyCycle(antialias(blue_c))
+    if pwm_mode == "HW":
+        ww.ChangeDutyCycle(ww_c)
+        cw.ChangeDutyCycle(cw_c)
+        red.ChangeDutyCycle(red_c)
+        green.ChangeDutyCycle(green_c)
+        blue.ChangeDutyCycle(blue_c)
+
+    elif pwm_mode == "SW":
+        ww.duty_cycle = hexscale(ww_c)
+        cw.duty_cycle = hexscale(cw_c)
+        red.duty_cycle = hexscale(red_c)
+        green.duty_cycle = hexscale(green_c)
+        blue.duty_cycle = hexscale(blue_c)
+
+    else:
+        print("pwm_mode error")
